b3392c2ee198b5f0b56058c5af2342d486086e6a
Daniel Power <me@danielpower.ca>
2023-07-11 02:19:24 -0230

    Update about page, run prettier (#4)
    * Update about page
    
    * Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,32 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'plugin:svelte/recommended',
-        'plugin:svelte/prettier'
-    ],
-    overrides: [
-        {
-            files: ['*.svelte'],
-            parser: 'svelte-eslint-parser',
-           parserOptions: {
-                parser: '@typescript-eslint/parser'
-            }
-        }
-    ],
-    plugins: ['@typescript-eslint'],
-    ignorePatterns: ['*.cjs'],
-    parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2020,
-        extraFileExtensions: ['.svelte']
-    },
-    env: {
-        browser: true,
-        es2017: true,
-        node: true
-    }
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:svelte/recommended',
+		'plugin:svelte/prettier'
+	],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	}
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,70 +1,70 @@
 <script lang="ts">
 	import 'prism-themes/themes/prism-nord.min.css';
-	import {page} from '$app/stores';
+	import { page } from '$app/stores';
 
 	const menuItems = [
-        {title: 'About', url: '/'},
-        {title: 'Blog', url: '/blog'},
-        {title: 'Projects', url: '/projects'},
-        {title: 'Resume', url: '/resume'},
-    ];
+		{ title: 'About', url: '/' },
+		{ title: 'Blog', url: '/blog' },
+		{ title: 'Projects', url: '/projects' },
+		{ title: 'Resume', url: '/resume' }
+	];
 </script>
 
 <layout>
-    <header>
-        <dan>Daniel Power</dan>
-        <nav>
-            {#each menuItems as menuItem}
-                <a class:active={menuItem.url === $page.route.id} href={menuItem.url}>{menuItem.title}</a>
-            {/each}
-        </nav>
-    </header>
-    <content>
-        <slot/>
-    </content>
-    <footer>Copyright © 2023, Daniel Power</footer>
+	<header>
+		<dan>Daniel Power</dan>
+		<nav>
+			{#each menuItems as menuItem}
+				<a class:active={menuItem.url === $page.route.id} href={menuItem.url}>{menuItem.title}</a>
+			{/each}
+		</nav>
+	</header>
+	<content>
+		<slot />
+	</content>
+	<footer>Copyright © 2023, Daniel Power</footer>
 </layout>
 
 <style>
-    layout {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        height: 100%;
-    }
+	layout {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		height: 100%;
+	}
 
-    header {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-    }
+	header {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
 
-    dan {
-        font-size: 2rem;
-    }
+	dan {
+		font-size: 2rem;
+	}
 
-    nav {
-        display: flex;
-        gap: 1rem;
-        width: 300px;
-    }
+	nav {
+		display: flex;
+		gap: 1rem;
+		width: 300px;
+	}
 
-    nav .active {
-        font-weight: 700;
-    }
+	nav .active {
+		font-weight: 700;
+	}
 
-    content {
-        height: 100%;
-        flex-grow: 1;
-    }
+	content {
+		height: 100%;
+		flex-grow: 1;
+	}
 
-    footer {
-        margin: 0 auto 0 auto;
-    }
+	footer {
+		margin: 0 auto 0 auto;
+	}
 
-    @media print {
-        nav {
-            display: none;
-        }
-    }
+	@media print {
+		nav {
+			display: none;
+		}
+	}
 </style>
