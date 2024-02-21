4f6d8566e793b4326de3eb6d87770c6cb5dfdc05
Daniel Power <me@danielpower.ca>
2024-02-21 01:21:01 -0330

    2024 week 7

---
+++ src/components/Image.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let src: string;
+	export let alt: string;
+</script>
+
+<figure>
+	<img {src} {alt} />
+	{#if $$slots.default}
+		<figcaption><slot /></figcaption>
+	{/if}
+</figure>
+
+<style>
+	figure {
+		max-width: 40rem;
+	}
+	img {
+		display: block;
+		max-width: 100%;
+	}
+</style>
