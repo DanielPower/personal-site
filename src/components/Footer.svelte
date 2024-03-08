d2a61f70765bc64f6038a2db8a4aa0ee369c4ccc
Daniel Power <me@danielpower.ca>
2024-03-08 19:35:42 -0330

    Refactor layout into components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	const year = new Date().getFullYear();
+</script>
+
+<footer>Copyright © {year}, Daniel Power</footer>
+
+<style>
+	footer {
+		text-align: center;
+	}
+
+	@media print {
+		footer {
+			display: none;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,18 @@
 <script lang="ts">
 	import 'prism-themes/themes/prism-nord.min.css';
-	import { page } from '$app/stores';
-
-	const menuItems = [
-		{ title: 'About', url: '/' },
-		{ title: 'Blog', url: '/blog' },
-		{ title: 'Projects', url: '/projects' },
-		{ title: 'Resume', url: '/resume' }
-	];
-
-	const year = new Date().getFullYear();
+	import Navbar from '../components/Navbar.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <div class="container">
 	<div class="topbar">
 		<span class="name">Daniel Power</span>
-		<nav>
-			{#each menuItems as menuItem}
-				<a
-					class:active={menuItem.url === $page.route.id}
-					href={menuItem.url}
-					data-text={menuItem.title}>{menuItem.title}</a
-				>
-			{/each}
-		</nav>
+		<Navbar />
 	</div>
 	<div class="content">
 		<slot />
 	</div>
-	<footer>Copyright © {year}, Daniel Power</footer>
+	<Footer />
 </div>
 
 <style>
@@ -58,47 +42,6 @@
 		display: flex;
 		align-items: end;
 		gap: 1rem;
-	}
-
-	nav {
-		display: flex;
-		gap: 1rem;
-	}
-
-	nav a {
-		position: relative;
-		text-align: center;
-	}
-
-	nav a.active {
-		visibility: hidden;
-	}
-
-	nav a::after {
-		content: attr(data-text);
-		position: absolute;
-		visibility: hidden;
-		left: 50%;
-		top: 0;
-		transform: translateX(-50%);
-		font-weight: 700;
-	}
-
-	nav a.active::after {
-		visibility: visible;
-	}
-
-	footer {
-		text-align: center;
-	}
-
-	@media print {
-		nav {
-			display: none;
-		}
-		footer {
-			display: none;
-		}
 	}
 
 	@media (max-width: 650px) {
