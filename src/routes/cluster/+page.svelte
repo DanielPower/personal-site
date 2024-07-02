d98ea9cb22020f52a936c345995b120a951d09db
Daniel Power <me@danielpower.ca>
2024-07-01 23:44:32 -0230

    Fix secret cluster page

--- src/routes/cluster/+page.svelte
+++ src/routes/cluster/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { env } from "$env/dynamic/public";
 	import Image from "$lib/components/Image.svelte";
-	import type { PageData } from "./$types";
-
-	export let data: PageData;
 </script>
 
-This page is being served by {data.hostname} in my homelab Kubernetes cluster. Try
-refreshing the page to see if you get a different node.
+This page is being served by {env.PUBLIC_NODE_NAME} in my homelab Kubernetes cluster.
+Try refreshing the page to see if you get a different node.
 
-<Image src={`nodes/${data.hostname}.png`} alt={data.hostname} />
+<Image
+	src={`nodes/${env.PUBLIC_NODE_NAME}.png`}
+	alt={env.PUBLIC_NODE_NAME || "Unknown node"}
+/>
