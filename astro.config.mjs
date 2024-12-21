24889ba0cae5d6d8c3718e3f3b06768a2acb7509
Daniel Power <me@danielpower.ca>
2024-12-20 23:11:58 -0330

    Fix canonical URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://danielpower.ca",
   integrations: [mdx(), sitemap(), icon()],
 
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
 });
+
