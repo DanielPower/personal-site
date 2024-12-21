0695c312b57689c9c46a9a015e3714ff2246897a
Daniel Power <me@danielpower.ca>
2024-12-20 23:24:21 -0330

    Fix date rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import icon from "astro-icon";
-
 import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://danielpower.ca",
   integrations: [mdx(), sitemap(), icon()],
